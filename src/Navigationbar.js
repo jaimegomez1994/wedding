4ec5d11cf3dfca0fed82f4a5268255a65edc48f8
Jaime-Gomez <Jaime.Gomez@coxautoinc.com>
2020-11-06 22:07:44 -0600

    initial commit

---
+++ src/Navigationbar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./saas/parent.scss";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const ShowNavigation = () => {
+    var menu = document.getElementById("navigation-container");
+    var menuMobile = document.getElementById("nav");
+    console.log('menu',menu);
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+        menuMobile.style.width = "0";
+    } else {
+        menu.style.display = "block";
+        menuMobile.style.width = "230px";
+    }
+}
+
+const Navigationbar = () => { 
+    return (
+        <>
+        <div id="nav" className="navigation navigation-mobile d-flex">
+            <a class="icon-container" >
+                <FontAwesomeIcon
+                    className="menu-icon"
+                    icon={faBars}
+                    onClick={ ShowNavigation}
+                    //swipe left
+                />
+            </a>
+            <div id="navigation-container" className="navigation-container">
+                <div className="navigation-top">H + J</div>
+                <ul id="nav" className="navigation-bottom">
+                    <li><a href="#home">¡Bienvenidos!</a></li>
+                    <li><a href="#home2">Confirma tu asistencia</a></li>
+                    <li><a href="#home3">Direccion</a></li>
+                    <li><a href="#home4">Mesa de regalos</a></li>
+                </ul>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default Navigationbar;
---
+++ src/Pages.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./saas/parent.scss";
+
+const Pages = () => { 
+    return (
+        <>
+        <div id="home" className="first-page">
+            <div className="names">
+                HANNY & JAIME
+            </div>
+            <div className="fecha">
+                Sábado 26 de diciembre 2020
+            </div>
+        </div>
+        <div id="home2" className="first-page">
+            <div className="names">
+                HANNY & JAIME
+            </div>
+            <div className="fecha">
+                Sábado 26 de diciembre 2020
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default Pages;
